1172597e0d29a29746cdcf2a6180477e9986478f
rmadidntwakeup <madramov.02@gmail.com>
2026-03-25 22:00:57 +0300

    ssh controller migrations

---
+++ platform/migrations/000002_create_ssh_connection_table.up.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+SET TIMEZONE="Europe/Moscow";
+
+CREATE TABLE ssh_connections (
+    id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
+    name VARCHAR (255) NOT NULL UNIQUE,
+    host TIMESTAMP WITH TIME ZONE DEFAULT NOW (),
+    user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    password TIMESTAMP NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW (),
+    updated_at TIMESTAMP NULL
+);
