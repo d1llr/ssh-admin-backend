12a80731d1a5a804b7145126a6a89bcfcb037048
rmadidntwakeup <madramov.02@gmail.com>
2026-03-29 18:16:04 +0300

    fixed migrations

--- platform/migrations/000002_create_ssh_connection_table.up.sql
+++ platform/migrations/000002_create_ssh_connection_table.up.sql
@@ -4,10 +4,10 @@ SET TIMEZONE="Europe/Moscow";
 
 CREATE TABLE ssh_connections (
     id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
-    name VARCHAR (255) NOT NULL UNIQUE,
-    host VARCHAR (255) NOT NULL UNIQUE,
+    name VARCHAR (255) NOT NULL,
+    host VARCHAR (255) NOT NULL,
     user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    password VARCHAR (255) NOT NULL UNIQUE,
+    password VARCHAR (255) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW (),
     updated_at TIMESTAMP NULL
 );
